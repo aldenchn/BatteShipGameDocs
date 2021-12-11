ca11aac415001282b7b9a0a72958442225ef293f
aldenchn <88219993+aldenchn@users.noreply.github.com>
2021-12-10 21:35:17 -0500

    Add files via upload

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Battleship"},{"p":"<Unnamed>","l":"Cruiser"},{"p":"<Unnamed>","l":"Destroyer"},{"p":"<Unnamed>","l":"EmptySea"},{"p":"<Unnamed>","l":"Ocean"},{"p":"<Unnamed>","l":"OceanInterface"},{"p":"<Unnamed>","l":"Ship"},{"p":"<Unnamed>","l":"Submarine"}];updateSearchResults();
